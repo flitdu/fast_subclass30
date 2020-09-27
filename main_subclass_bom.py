d1f37c814dffcba229026c7d48b66761f1fed878
dufy <dufy.du@allchips.com>
2020-09-27 10:20:34 +0800

    规则

--- main_subclass_bom.py
+++ main_subclass_bom.py
@@ -300,7 +300,7 @@ class TestExcel(OperateExcel):  # 重写函数
                     return 1, '钽电容'
                 elif subclass_label_i == '贴片电容' and bool(re.search(r'\b5\s\*\s5.4\b', content)):  # 封装不对
                     continue
-                elif subclass_label_i == '电容器阵列与网络' and bool(re.search(r'\b0805\b', content)):  # 封装不对
+                elif subclass_label_i == '电容器阵列与网络' and bool(re.search(r'\b0805|0603\b', content)):  # 封装不对
                     continue
             elif entity_label == '传感器':
                 if bool(re.search(r'(\bcompass\b)', content)):
