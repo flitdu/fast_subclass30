cac1d4c525d77db001f383ebc9b915fc48d4046d
dufy <dufy.du@allchips.com>
2020-11-12 17:50:21 +0800

    规则--钽电容二级判断修改

--- main_subclass_bom.py
+++ main_subclass_bom.py
@@ -330,6 +330,8 @@ class TestExcel(OperateExcel):  # 重写函数
                     continue
                 elif (subclass_label_i == '贴片电解电容' or subclass_label_i == '薄膜电容') and bool(re.search(r'\b1206\b', content)):  # 封装不对
                     continue
+                elif (subclass_label_i == '钽电容') and bool(re.search(r'\b1812\b', content)):  # 封装不对
+                    continue
 
             elif entity_label == '传感器':
                 if bool(re.search(r'(\bcompass\b)', content)):
